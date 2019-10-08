3245188729a51980c6b89d92f2d022f827480208
David Patch <dop4398@rit.edu>
2019-10-08 12:11:11 -0400

    uhhhh FPS scene

---
+++ Assets/SpookyGame/Assets/Scripts/FPSController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSController : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        PlayerMovement();
+    }
+
+    void PlayerMovement()
+    {
+        if(Input.GetKey(KeyCode.W))
+        {
+            //gameObject.transform.Translate(
+            //    gameObject.transform.localPosition.x,
+            //    gameObject.transform.localPosition.y + 0.001f,
+            //    gameObject.transform.localPosition.z
+            //    );
+            gameObject.transform.Translate(
+                0.0f,
+                0.01f,
+                0.0f
+                );
+        }
+    }
+}
