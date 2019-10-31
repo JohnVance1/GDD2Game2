56da97bd98ac6802dcc1c92a3e515287b0bf4b4a
David <dop4398@rit.edu>
2019-10-31 01:48:15 -0400

    Got pause menu mostly working

--- Assets/SpookyGame/Assets/Scripts/GameManager.cs
+++ Assets/SpookyGame/Assets/Scripts/GameManager.cs
@@ -28,17 +28,22 @@ public class GameManager : MonoBehaviour
             TogglePauseMenu();
     }
 
+    /// <summary>
+    /// Toggles the pause menu with either the 'Escape' key or 'Resume' button.
+    /// </summary>
     public void TogglePauseMenu()
     {
         if(this.GetComponentInChildren<Canvas>().enabled)
         {
             this.GetComponentInChildren<Canvas>().enabled = false;
             Time.timeScale = 1.0f;
+            Cursor.lockState = CursorLockMode.Locked;
         }
         else
         {
             this.GetComponentInChildren<Canvas>().enabled = true;
             Time.timeScale = 0.0f;
+            Cursor.lockState = CursorLockMode.Confined;
         }
     }
 }
