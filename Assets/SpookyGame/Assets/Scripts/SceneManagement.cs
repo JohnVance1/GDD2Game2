0438494fb6e9c28852ce5557fe204fbac145bb7b
Drake Richards <dwr2966@rit.edu>
2019-10-31 02:09:57 -0400

    little uimanager stuff

--- Assets/SpookyGame/Assets/Scripts/SceneManagement.cs
+++ Assets/SpookyGame/Assets/Scripts/SceneManagement.cs
@@ -18,8 +18,8 @@ public class SceneManagement : MonoBehaviour, IPointerEnterHandler, IPointerExit
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(button);
-        if (Input.GetMouseButtonDown(0))
+        //Debug.Log(button);
+        if(Input.GetMouseButtonDown(0))
         {
             menuAudio.GetComponent<AudioSource>().volume = 1.0f;
 
@@ -27,49 +27,36 @@ public class SceneManagement : MonoBehaviour, IPointerEnterHandler, IPointerExit
             {
                 menuAudio.GetComponent<AudioSource>().clip = (AudioClip)Resources.Load("BonyBoy_ButtonSelect");
                 menuAudio.GetComponent<AudioSource>().Play();
+                //if (Input.GetMouseButtonUp(0))
+                    SceneManager.LoadScene(2);
             }
             else if (button == "MenuButton")
             {
                 menuAudio.GetComponent<AudioSource>().clip = (AudioClip)Resources.Load("BonyBoy_ButtonSelect");
                 menuAudio.GetComponent<AudioSource>().Play();
+                //if (Input.GetMouseButtonUp(0))
+                    SceneManager.LoadScene(0);
             }
             else if (button == "CreditsButton")
             {
                 menuAudio.GetComponent<AudioSource>().clip = (AudioClip)Resources.Load("BonyBoy_ButtonSelect");
                 menuAudio.GetComponent<AudioSource>().Play();
+                //if (Input.GetMouseButtonUp(0))
+                    SceneManager.LoadScene(1);
             }
             else if (button == "ResumeButton")
             {
                 menuAudio.GetComponent<AudioSource>().clip = (AudioClip)Resources.Load("BonyBoy_ButtonSelect");
                 menuAudio.GetComponent<AudioSource>().Play();
+                //if (Input.GetMouseButtonUp(0))
+                    GameObject.FindGameObjectWithTag("UI").GetComponent<GameManager>().TogglePauseMenu();
             }
             else if (button == "QuitButton")
             {
                 menuAudio.GetComponent<AudioSource>().clip = (AudioClip)Resources.Load("BonyBoy_ButtonSelect");
                 menuAudio.GetComponent<AudioSource>().Play();
-            }
-        }
-        if (Input.GetMouseButtonUp(0))
-        {
-            if (button == "PlayButton")
-            {
-                SceneManager.LoadScene(2);
-            }
-            else if (button == "MenuButton")
-            {
-                SceneManager.LoadScene(0);
-            }
-            else if (button == "CreditsButton")
-            {
-                SceneManager.LoadScene(1);
-            }
-            else if (button == "ResumeButton")
-            {
-                GameObject.FindGameObjectWithTag("UI").GetComponent<GameManager>().TogglePauseMenu();
-            }
-            else if (button == "QuitButton")
-            {
-                Application.Quit();
+                //if (Input.GetMouseButtonUp(0))
+                    Application.Quit();
             }
         }
     }
@@ -77,7 +64,7 @@ public class SceneManagement : MonoBehaviour, IPointerEnterHandler, IPointerExit
     public void OnPointerEnter(PointerEventData pointerEventData)
     {
         button = name;
-        menuAudio.GetComponent<AudioSource>().volume = 0.4f;
+        menuAudio.GetComponent<AudioSource>().volume = 0.5f;
         menuAudio.GetComponent<AudioSource>().clip = (AudioClip)Resources.Load("BonyBoy_ButtonHover");
         menuAudio.GetComponent<AudioSource>().Play();
     }
@@ -85,7 +72,7 @@ public class SceneManagement : MonoBehaviour, IPointerEnterHandler, IPointerExit
     public void OnPointerExit(PointerEventData pointerEventData)
     {
         button = "";
-        menuAudio.GetComponent<AudioSource>().volume = 0.2f;
+        menuAudio.GetComponent<AudioSource>().volume = 0.5f;
         menuAudio.GetComponent<AudioSource>().clip = (AudioClip)Resources.Load("BonyBoy_ButtonHover");
         menuAudio.GetComponent<AudioSource>().Play();
     }
